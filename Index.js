5505ef0912687c120797c87981df16c7506892ec
embeddedscript <richarddevonwilliams@gmail.com>
2017-02-15 21:08:01 -0500

    Java Experiments
    Java experiments with loops and if statements, as well as linking CSS
    to java statements

--- Index.js
+++ Index.js
@@ -1,1 +1,36 @@
-Gonna be some Java up in hurr
+var count = 0;
+var score1 = 0;
+var score2 = 0;
+var score3 = 0;
+var ticker1 = 0;
+var ticker2 = 0;
+
+for (var count = 0; count <11; count++){
+  score1 = count;
+  score2 = count;
+  score3 = count;
+}
+
+if (count = 3){
+  ticker1 = [count-2]
+}
+
+if (count = 6){
+  ticker2 = [count-4]
+}
+
+if (score1 = 3){
+  document.write('<div class="container"><p class="red">Red' + ticker1 + ' is equal to ' + score1 + '</p></div>')
+}
+
+if (score1 = 6){
+  document.write('<div class="container"><p class="red">Red' + ticker2 + ' is equal to ' + score1 + '</p></div>')
+}
+
+if (score2 = 8){
+  document.write('<div class="container"><p class="green">Green is equal to ' + score2 + '</p></div>')
+}
+
+if (score3 = 10){
+  document.write('<div class="container"><p class="blue">Blue is equal to ' + score3 + '</p></div>')
+}
